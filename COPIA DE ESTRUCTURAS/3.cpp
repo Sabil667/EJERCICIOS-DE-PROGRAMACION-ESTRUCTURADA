e5ed0a6c2ecd8f44221820f51f0b6980aa5c76b6
Sabil667 <msabi@myuax.com>
2023-12-12 21:19:10 +0100

    .

--- COPIA DE ESTRUCTURAS/3.cpp
+++ COPIA DE ESTRUCTURAS/3.cpp
@@ -14,3 +14,13 @@ Estudiante copiarEstudiante(const Estudiante& est) {
     nuevoEst.promedio = est.promedio;
     return nuevoEst;
 }
+
+int main() {
+    // Crear una estructura Estudiante
+    Estudiante estudianteOriginal;
+    std::cout << "Ingrese el nombre del estudiante: ";
+    std::cin.getline(estudianteOriginal.nombre, 50);
+    std::cout << "Ingrese la edad del estudiante: ";
+    std::cin >> estudianteOriginal.edad;
+    std::cout << "Ingrese el promedio del estudiante: ";
+    std::cin >> estudianteOriginal.promedio;
