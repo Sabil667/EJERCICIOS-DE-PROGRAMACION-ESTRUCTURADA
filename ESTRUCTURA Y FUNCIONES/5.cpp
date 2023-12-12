31b3ebebe61219606bbec4db7a9dfee4ee2167e7
Sabil667 <msabi@myuax.com>
2023-12-12 21:38:46 +0100

    .

--- ESTRUCTURA Y FUNCIONES/5.cpp
+++ ESTRUCTURA Y FUNCIONES/5.cpp
@@ -13,3 +13,28 @@ struct Estudiante {
 void imprimirEstudiantePorValor(Estudiante est) {
     std::cout << "Nombre: " << est.nombre << "\nEdad: " << est.edad << "\nPromedio: " << est.promedio << std::endl;
 }
+
+// Función que toma una estructura por referencia
+void imprimirEstudiantePorReferencia(const Estudiante& est) {
+    std::cout << "Nombre: " << est.nombre << "\nEdad: " << est.edad << "\nPromedio: " << est.promedio << std::endl;
+}
+
+// Función que toma una dirección a una estructura
+void imprimirEstudiantePorDireccion(const Estudiante* est) {
+    std::cout << "Nombre: " << est->nombre << "\nEdad: " << est->edad << "\nPromedio: " << est->promedio << std::endl;
+}
+
+// Clase Estudiante para la programación orientada a objetos
+class EstudianteObjeto {
+public:
+    // Constructor
+    EstudianteObjeto(const char* nombre, int edad, float promedio) {
+        strcpy(this->nombre, nombre);
+        this->edad = edad;
+        this->promedio = promedio;
+    }
+
+    // Método para imprimir la información del estudiante
+    void imprimirInformacion() const {
+        std::cout << "Nombre: " << nombre << "\nEdad: " << edad << "\nPromedio: " << promedio << std::endl;
+    }
