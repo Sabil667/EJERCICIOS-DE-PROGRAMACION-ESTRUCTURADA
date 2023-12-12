fde9f135f7b0b8e974ebb51d872218ee89f1f988
Sabil667 <msabi@myuax.com>
2023-12-12 21:18:54 +0100

    .

---
+++ COPIA DE ESTRUCTURAS/3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
+
+Estudiante copiarEstudiante(const Estudiante& est) {
+    Estudiante nuevoEst;
+    strcpy(nuevoEst.nombre, est.nombre);
+    nuevoEst.edad = est.edad;
+    nuevoEst.promedio = est.promedio;
+    return nuevoEst;
+}
